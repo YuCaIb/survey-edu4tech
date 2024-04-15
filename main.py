9975a8fce9732c648c6ff22c5fa74bf5f9cd0612
yucaib <y.canfee@gmail.com>
2024-04-15 22:49:53 +0200

    tamplate creater, added login register (almost)

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, jsonify, session
 import pymongo
+from werkzeug.security import generate_password_hash, check_password_hash
+
 import turkish_rake as yucaib
 
 app = Flask(__name__)
+app.secret_key = 'mysecretkey'
 
 # MongoDB connect
 client = pymongo.MongoClient()
@@ -24,7 +27,7 @@ def get_sequence(seq_name):
     return sequence_document["seq"]
 
 
-@app.route("/add-db", methods=["POST"])
+@app.route("/home", methods=["POST"])
 def parse_to_db():
     try:
         corpus = request.form.get("corpus")
@@ -34,16 +37,57 @@ def parse_to_db():
                                  "_id": get_sequence('Corpus'),
                                  'keywords': keywords})
 
-        return redirect("/home.html" + corpus + keywords)
+        return render_template("home.html", keywords=keywords)
 
     except Exception as e:
         return f"An error occurred: {e}"
 
 
-@app.route("/", methods=['GET'])
-def keywords_paste(corpus):
-    result = db.list_collections()
-    return 0
+@app.route('/kayit-ol', methods=["GET", "POST"])
+def uye_ol():
+    if request.method == 'GET':
+        return render_template("kayit-ol.html")
+    else:
+        # Formdan gelen verileri al
+        email = request.form["email"]
+        sifre = request.form["sifre"]
+        adsoyad = request.form["adsoyad"]
+
+        hashed_sifre = generate_password_hash(sifre, method='pbkdf2:sha256')
+
+        # Verileri collection'a ekle
+        db["kullanicilar"].insert_one({
+            "_id": email,
+            "sifre": hashed_sifre,
+            "adsoyad": adsoyad
+        })
+
+        return redirect("/giris", 302)
+
+@app.route('/giris', methods=["GET", "POST"])
+def giris():
+    if request.method == 'GET':
+        return render_template("giris.html")
+    else:
+        # Formdan gelen verileri al
+        email = request.form["email"]
+        sifre = request.form["sifre"]
+
+        kullanici = db["kullanicilar"].find_one({"_id": email})
+        print("kullanici:", kullanici)
+
+        if kullanici and check_password_hash(kullanici["sifre"], sifre):
+            session['kullanici'] = kullanici
+            return redirect("/", 302)
+        else:
+            return "Kullanıcı bulunamadı ya da şifre geçersiz"
+
+
+@app.route('/cikis', methods=["GET", "POST"])
+def cikis():
+    session.pop('kullanici', None)
+    return redirect("/", 302)
+
 
 
 if __name__ == '__main__':
