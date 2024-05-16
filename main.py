19e1282deb229cd92a6cb2a99458fbf2ab0dfd03
yucaib <y.canfee@gmail.com>
2024-05-17 00:29:29 +0200

    has done create survey and survey details also added funcionaltiy

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, jsonify, session
+from flask import Flask, render_template, request, redirect, jsonify, session, url_for
 import pymongo
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
-import turkish_rake as yucaib
+
+# import turkish_rake as yucaib
 
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
@@ -10,11 +11,54 @@ app.secret_key = 'mysecretkey'
 # MongoDB connect
 client = pymongo.MongoClient()
 db = client["KeywordDB"]
+survey_collection = db['survey']
+
 
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    surveys = list(survey_collection.find())
+
+    return render_template("home.html", surveys=surveys)
+
+
+@app.route('/survey/<survey_id>')
+def survey_details(survey_id):
+    # Seçilen anketi al
+    survey = survey_collection.find_one({'_id': survey_id})
+
+    # Anketi html'e gönder
+    return render_template('survey_detail.html', survey=survey)
+
+
+# @app.route('/survey/<string:survey_id>')
+# def survey_detail(survey_id):
+#   # Get survey detail from MongoDB
+#  survey_detail = db.survey.find_one({"_id": survey_id})
+# return render_template('survey_detail.html', survey_detail=survey_detail)
+
+
+@app.route('/update_count', methods=['POST','GET'])
+def update_count():
+    option_id = request.json['option_id']
+    survey_id = request.json['survey_id']
+    print("survey_id : ", survey_id)
+    print("option_id : ", option_id)
+    survey = survey_collection.find_one({'_id':survey_id})
+
+    if survey:
+        # count'u arttır
+        for option in survey['options']:
+            if option['id'] == option_id:
+                option['count'] += 1
+                break
+
+        # Değişikliği kaydet
+        survey_collection.update_one({"_id": survey_id}, {"$set": {"options": survey['options']}})
+        updated_count = next(option['count'] for option in survey['options'] if option['id'] == option_id)
+        return jsonify({'message': 'Count incremented successfully', 'updated_count': updated_count}), 200
+    else:
+        return jsonify({'error': 'Survey not found'}), 404
 
 
 def get_sequence(seq_name):
@@ -27,20 +71,20 @@ def get_sequence(seq_name):
     return sequence_document["seq"]
 
 
-@app.route("/home", methods=["POST"])
-def parse_to_db():
-    try:
-        corpus = request.form.get("corpus")
-        keywords = yucaib.key_word_ext(corpus=corpus, keyword_count=10)
-
-        db['Corpus'].insert_one({"corpus": corpus,
-                                 "_id": get_sequence('Corpus'),
-                                 'keywords': keywords})
-
-        return render_template("home.html", keywords=keywords)
-
-    except Exception as e:
-        return f"An error occurred: {e}"
+# @app.route("/home", methods=["POST"])
+# def parse_to_db():
+#     try:
+#         corpus = request.form.get("corpus")
+#         keywords = yucaib.key_word_ext(corpus=corpus, keyword_count=10)
+#
+#         db['Corpus'].insert_one({"corpus": corpus,
+#                                  "_id": get_sequence('Corpus'),
+#                                  'keywords': keywords})
+#
+#         return render_template("home.html", keywords=keywords)
+#
+#     except Exception as e:
+#         return f"An error occurred: {e}"
 
 
 @app.route('/kayit-ol', methods=["GET", "POST"])
@@ -83,6 +127,7 @@ def giris():
         else:
             return "Kullanıcı bulunamadı ya da şifre geçersiz"
 
+
 @app.route('/create-survey', methods=['GET', 'POST'])
 def survey():
     if request.method == 'GET':
@@ -97,12 +142,13 @@ def survey():
             # Oylama maddelerini al
             num_options = int(request.form['numOptions'])
             survey_id = uuid.uuid1().hex
-            options = [{"text": request.form[f'option{i + 1}'], "count": 0,'survey_id': survey_id}
-                       for i in range(num_options)]
+            options = [
+                {"text": request.form[f'option{i + 1}'], "count": 0, 'survey_id': survey_id, 'id': uuid.uuid1().hex}
+                for i in range(num_options)]
 
             # Anket verilerini MongoDB'ye kaydet
             db["survey"].insert_one({
-                "_id": survey_id ,
+                "_id": survey_id,
                 "anket_basligi": anket_basligi,
                 "options": options
             })
@@ -114,13 +160,10 @@ def survey():
             return jsonify({'error': f'Hata oluştu: {str(e)}'})
 
 
-
 @app.route('/cikis', methods=["GET", "POST"])
 def cikis():
     session.pop('kullanici', None)
     return redirect("/", 302)
-
-
 
 
 if __name__ == '__main__':
